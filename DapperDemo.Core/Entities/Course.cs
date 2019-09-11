b6e53b3dd713705386e6939a7ac13695634545b9
Antonio <achamorro@wecodeteam.com>
2019-09-11 13:06:14 +0200

    Initial commit. First Entities

---
+++ DapperDemo.Core/Entities/Course.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DapperDemo.Core.Entities
+{
+    public class Course
+    {
+        private readonly IList<Student> _students= new List<Student>();
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IList<Student> Students => _students;
+
+        public Course()
+        {
+            
+        }
+
+        public Student GetStudent(int id)
+        {
+            return _students.First(s => s.Id == id);
+        }
+
+        public void AddStudent(Student newStudent)
+        {
+            _students.Add(newStudent);
+        }
+
+        public bool RemoveStudent(Student student)
+        {
+            return _students.Remove(student);
+        }
+    }
+}
---
+++ DapperDemo.Core/Entities/Student.cs
@@ -0,0 +1,9 @@
+namespace DapperDemo.Core.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Nif { get; set; }
+    }
+}

87e55ac8d56dae884dadaa425317ca53b1f8132d
Antonio <achamorro@wecodeteam.com>
2019-09-11 13:46:52 +0200

    First entities

--- DapperDemo.Core/Entities/Course.cs
+++ DapperDemo.Core/Entities/Course.cs
@@ -1,36 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
 
 namespace DapperDemo.Core.Entities
 {
     public class Course
     {
-        private readonly IList<Student> _students= new List<Student>();
-
-        public int Id { get; set; }
+        public string Code { get; set; }
         public string Name { get; set; }
-
-        public IList<Student> Students => _students;
-
-        public Course()
-        {
-            
-        }
-
-        public Student GetStudent(int id)
-        {
-            return _students.First(s => s.Id == id);
-        }
-
-        public void AddStudent(Student newStudent)
-        {
-            _students.Add(newStudent);
-        }
-
-        public bool RemoveStudent(Student student)
-        {
-            return _students.Remove(student);
-        }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
     }
 }
--- DapperDemo.Core/Entities/Student.cs
+++ DapperDemo.Core/Entities/Student.cs
@@ -1,9 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace DapperDemo.Core.Entities
 {
     public class Student
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Nif { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IList<Course> RegisteredCourses { get; set; }
+        public IList<Course> EnrolledCourses { get; set; }
+
+        public Student()
+        {
+            RegisteredCourses = new List<Course>();
+            EnrolledCourses = new List<Course>();
+        }
+
+        public bool RegisterForCourse(Course course)
+        {
+            // student has not previously enrolled
+            if (EnrolledCourses.Any(ec => ec.Code == course.Code)) return false;
+
+            // student has not previously registered
+            if (RegisteredCourses.Any(ec => ec.Code == course.Code)) return false;
+
+            // registratraion cannot occur with 5 days of course start date
+            if (DateTime.UtcNow > course.StartDate.AddDays(-5)) return false;
+
+            RegisteredCourses.Add(course);
+            return true;
+        }
     }
 }
